eb84fde1a521c7f63a440cd4cc5cd43850ca40b7
gabrielyat-sketch <ramirogabriel5032@gmail.com>
2024-11-03 21:35:47 -0600

    Avances

--- inv/models.py
+++ inv/models.py
@@ -12,12 +12,94 @@ class Categoria(ClaseModelo): #hereda de moder.url
     def _str_(self):
         return '()'.format(self.descripcion)
     
-        def save(self):
-            self.descripcion = self.descripcion.Upper() #Guarda los datos y los deja en mayuscula en el servidor.
-            super(Categoria, self).save()
+    def save(self):
+        self.descripcion = self.descripcion.Upper() #Guarda los datos y los deja en mayuscula en el servidor.
+        super(Categoria, self).save()
 
-            class Meta:
-                verbose_name_plural= "Categorias"  #Guarda en la clase Categorias.
+    class Meta:
+        verbose_name_plural= "Categorias"  #Guarda en la clase Categorias.
+
+class Subcategoria(ClaseModelo):
+    Categoria=models.Foreignkey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripcion de la Categoria',
+
+    )
+
+    def __str__(self):
+        return '{}:{}'.format(self.categoriadescripcion,self.descripcion)
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Subcategoria, self).save()
+    
+    class Meta:
+        verbose_name_plural="Sub Categorias"
+        unique_together =('categoria','descripcion')
+
+class Marca(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la Marca',
+        unique=True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Marca, self).save()
+
+    class Meta:
+        verbose_name_plural = "Marca"
+
+
+class UnidadMedida(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la Unidad Medida',
+        unique=True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(UnidadMedida, self).save()
+
+    class Meta:
+        verbose_name_plural = "Unidades de Medida"
+
+
+class Producto(ClaseModelo):
+    codigo = models.CharField(
+        max_length=20,
+        unique=True
+    )
+    codigo_barra = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    precio = models.FloatField(default=0)
+    existencia = models.IntegerField(default=0)
+    ultima_compra = models.DateField(null=True, blank=True)
+
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    unidad_medida = models.ForeignKey(UnidadMedida, on_delete=models.CASCADE)
+    subcategoria = models.ForeignKey(Subcategoria, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to="images/",null=True,blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Producto,self).save()
+    
+    class Meta:
+        verbose_name_plural = "Productos"
+        unique_together = ('codigo','codigo_barra')
 
 
 
--- inv/views.py
+++ inv/views.py
@@ -1,3 +1,100 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from django.urls import reverse_lazy
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Categoria,Subcategoria
+from .forms import CategoriaForm, SubCategoriaForm
 # Create your views here.
+
+class Categoriaview(LoginRequiredMixin, generic.ListView):
+    model = Categoria
+    template = "inv/categoria_list.html"
+    context_object_name = "obj"
+    login_url = 'bases:login'
+
+class CategoriaNew(LoginRequiredMixin, generic.CreateView):
+    model=Categoria
+    template_name="inv/categoria_form.html"
+    context_objet_name = "obj"
+    form_class=CategoriaForm
+    success_url=reverse_lazy("inv:categoria_list")
+    login_url="bases:login"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model=Categoria
+    template_name="inv/categoria_form.html"
+    context_objet_name = "obj"
+    form_class=CategoriaForm
+    success_url=reverse_lazy("inv:categoria_list")
+    login_url="bases:login"
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+class CategoriaDel(LoginRequiredMixin, generic.DeleteView):
+    model=Categoria
+    template_name='inv/catalogos_del.html'
+    context_object_name='obj'
+    success_url=reverse_lazy("inv:categoria_list")
+
+class SubCategoria(LoginRequiredMixin, generic.ListView):
+    model = Subcategoria
+    template = "inv/subcategoria_list.html"
+    context_object_name = "obj"
+    login_url = 'bases:login'
+
+class SubCategoriaNew(LoginRequiredMixin, generic.CreateView):  # Alineada correctamente
+    model = Subcategoria
+    template_name = "inv/subcategoria_form.html"
+    context_object_name = "obj"
+    form_class = SubCategoriaForm
+    success_url = reverse_lazy("inv:subcategoria_list")
+    login_url = "bases:login"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+class SubCategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model=SubCategoria
+    template_name="inv/subcategoria_form.html"
+    context_objet_name = "obj"
+    form_class=SubCategoriaForm
+    success_url=reverse_lazy("inv:subcategoria_list")
+    login_url="bases:login"
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+class SubCategoriaDel(LoginRequiredMixin, generic.DeleteView):
+    model=SubCategoria
+    template_name='inv/catalogos_del.html'
+    context_object_name='obj'
+    success_url=reverse_lazy("inv:subcategoria_list")
+
+
+def marca_inactivar(request,id ):
+    marca = marca.objects.filter(pk=id).first()
+    contexto={}
+    template_name="inv/catalogo_del.thml"
+
+    if not marca:
+        return redirect("inv:marca_list")
+    
+    if request.method=="GET":
+        contexto={'obj': marca}
+    
+    if request.metchod=='POST':
+        marca.estado=False
+        marca.save()
+        return redirect("inv:marca_list")
+
+    return render(request,template_name,contexto)
